07bcf93a7607da2b01fd2c7f0ef0b7ccb9896e80
tonyykosseifyy <tonykosseify123@gmail.com>
2024-02-17 17:13:51 +0200

    add utils functions for global project use

--- include/headers.h
+++ include/headers.h
@@ -10,3 +10,6 @@
 int getNumCPUs();
 int countFiles(const char *folderPath);
 char ** listFiles(const char *folderPath); 
+int directoryExists(const char *path);
+void createDirectory(const char *path);
+void compressFile(const char *inputFilePath, const char *outputDir);
